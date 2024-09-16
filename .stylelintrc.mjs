49ae0bae5ce7e92da7b15c0621ea945f0a6c544f
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2024-09-16 20:50:51 +0900

    fix: 設定ファイル系のフォーマット(#3)

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,13 +1,13 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
-  plugins: ["stylelint-order"],
-  ignoreFiles: ["**/node_modules/**"],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  plugins: ['stylelint-order'],
+  ignoreFiles: ['**/node_modules/**'],
   rules: {
-    "selector-class-pattern": [
-      "^([a-z][a-z0-9]*)(-[a-z0-9]+)*(__)*([a-z][a-z0-9]*)(-[a-z0-9]+)*(--)*([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
+    'selector-class-pattern': [
+      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*(__)*([a-z][a-z0-9]*)(-[a-z0-9]+)*(--)*([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
       {
-        message: "Expected class selector to be BEM",
+        message: 'Expected class selector to be BEM',
       },
     ],
   },
