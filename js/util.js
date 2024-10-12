f0b8b66c836aad77ce91e67dd70707c2b4cf5d5c
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2024-10-12 16:31:05 +0900

    feat: 配列シャッフル関数作成(#5)

--- js/util.js
+++ js/util.js
@@ -6,3 +6,18 @@
 export const getRandomNumber = (max) => {
   return Math.floor(Math.random() * max) + 1;
 };
+
+/**
+ * 配列をシャッフルしたものを取得する（ダステンフェルドの手法）
+ * @param {any[]} list
+ * @returns {any[]}
+ */
+export const getShuffledList = (list) => {
+  const cList = list.concat();
+  for (let i = cList.length; i > 1; i--) {
+    const k = Math.floor(Math.random() * i);
+    [cList[k], cList[i - 1]] = [cList[i - 1], cList[k]];
+  }
+
+  return cList;
+};
