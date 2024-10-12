15f810f7c1fc2f8e3cba74ccc98a2abd7d3b3d7d
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2024-10-12 21:44:05 +0900

    refactor: randomWordGame周りのリファクタ + JSDoc追加(#5)

--- js/game.js
+++ js/game.js
@@ -21,41 +21,43 @@ window.addEventListener('DOMContentLoaded', async () => {
   let randomWordGame;
 
   /**
-   * @param {RandomWordGame} randomWordGame ゲーム管理クラスのインスタンス
+   * 入力された1文字の回答確認
    * @param {number} index 何文字目か
    */
-  const handleAnswerCheck = (randomWordGame, index) => (e) => {
-    const answerWord = e.target.value;
-    // 何も入力されてないときは何もしない
-    if (answerWord === '') return;
+  const handleAnswerCheck = (index) => {
+    return (e) => {
+      const answerWord = e.target.value;
+      // 何も入力されてないときは何もしない
+      if (answerWord === '') return;
 
-    const currentInputElement = document.getElementById(
-      `answer-input-${index}`,
-    );
-    document.getElementById(`answer-input-${index + 1}`).focus();
-
-    if (randomWordGame.isAnswerCheckWord(answerWord, index)) {
-      currentInputElement.classList.add('question-card__form-input--correct');
-    } else {
-      currentInputElement.classList.remove(
-        'question-card__form-input--correct',
+      const currentInputElement = document.getElementById(
+        `answer-input-${index}`,
       );
-      randomWordGame.addMistakeWord(answerWord);
-      mistakeWordsElement.textContent = randomWordGame
-        .getMistakeWordList()
-        .join(', ');
-      randomWordGame.countUpTries();
-    }
+      document.getElementById(`answer-input-${index + 1}`).focus();
 
-    const triesCount = randomWordGame.getTriesCount();
-    if (triesCount >= 6) {
-      console.log('out');
-    } else {
-      triesCountElement.textContent = triesCount;
-      triesElementList[triesCount - 1].classList.add(
-        'question-card__tries-info-step--active',
-      );
-    }
+      if (randomWordGame.isAnswerCheckWord(answerWord, index)) {
+        currentInputElement.classList.add('question-card__form-input--correct');
+      } else {
+        currentInputElement.classList.remove(
+          'question-card__form-input--correct',
+        );
+        randomWordGame.addMistakeWord(answerWord);
+        mistakeWordsElement.textContent = randomWordGame
+          .getMistakeWordList()
+          .join(', ');
+        randomWordGame.countUpTries();
+      }
+
+      const triesCount = randomWordGame.getTriesCount();
+      if (triesCount >= 6) {
+        console.log('out');
+      } else {
+        triesCountElement.textContent = triesCount;
+        triesElementList[triesCount - 1].classList.add(
+          'question-card__tries-info-step--active',
+        );
+      }
+    };
   };
 
   /**
@@ -68,11 +70,10 @@ window.addEventListener('DOMContentLoaded', async () => {
 
   /**
    * 回答欄1つあたりを生成する
-   * @param {RandomWordGame} randomWordGame ゲーム管理クラスのインスタンス
    * @param {number} index インデックス
    * @returns {HTMLInputElement} 回答欄要素
    */
-  const createInputElement = (randomWordGame, index) => {
+  const createInputElement = (index) => {
     const inputElement = document.createElement('input');
     inputElement.id = `answer-input-${index}`;
     inputElement.classList.add('question-card__form-input');
@@ -81,11 +82,14 @@ window.addEventListener('DOMContentLoaded', async () => {
     inputElement.maxLength = '1';
     inputElement.ariaLabel = `Answer input field - ${index}`;
     inputElement.autocomplete = 'off';
-    inputElement.oninput = handleAnswerCheck(randomWordGame, index);
+    inputElement.oninput = handleAnswerCheck(index);
     inputElement.onfocus = handleFocusSelect;
     return inputElement;
   };
 
+  /**
+   * 出題内容の初期セットアップ
+   */
   const initialGame = async () => {
     const randomWordNumber = getRandomNumber(10);
     const randomWord = (await getRandomWord(randomWordNumber))[0];
@@ -95,7 +99,7 @@ window.addEventListener('DOMContentLoaded', async () => {
     [...Array(randomWordGame.getWordLength())].forEach((_, index) => {
       answerGroupElement.insertAdjacentElement(
         'beforeend',
-        createInputElement(randomWordGame, index + 1),
+        createInputElement(index + 1),
       );
     });
     questionElement.textContent = randomWordGame.getQuesitonWord();
@@ -104,6 +108,9 @@ window.addEventListener('DOMContentLoaded', async () => {
     document.getElementById('answer-input-1').focus();
   };
 
+  /**
+   * ゲーム状態をオールリセット
+   */
   const resetGame = () => {
     randomWordGame.allReset();
     triesCountElement.textContent = randomWordGame.getTriesCount();
