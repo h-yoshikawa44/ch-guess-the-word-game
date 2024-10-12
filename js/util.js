662b2d08e06cd62f23ba0ec50e7915103dc8a5d7
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2024-10-12 16:33:17 +0900

    fix: JSDocのparamに変数、返り値説明がなかったので追記(#5)

--- js/util.js
+++ js/util.js
@@ -1,7 +1,7 @@
 /**
  * 1～指定した数までのランダムな数値を取得する
- * @param {number} max
- * @returns {number}
+ * @param {number} max 最大値
+ * @returns {number} 1～指定した数までのランダムな数値
  */
 export const getRandomNumber = (max) => {
   return Math.floor(Math.random() * max) + 1;
@@ -9,8 +9,8 @@ export const getRandomNumber = (max) => {
 
 /**
  * 配列をシャッフルしたものを取得する（ダステンフェルドの手法）
- * @param {any[]} list
- * @returns {any[]}
+ * @param {any[]} list シャッフルしたい配列
+ * @returns {any[]} シャッフルされた配列
  */
 export const getShuffledList = (list) => {
   const cList = list.concat();
